aa51af06f5c5920fa1fb47277906525d1b0d6572
Olivia McKee <olivia.mckee97@gmail.com>
2022-09-01 17:56:49 -0500

    updating with code snapshot

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
-import Nav from './components/Nav'
 
 function App() {
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav />
       <main>
-        <About></About>
+        <About />
       </main>
     </div>
   );
 }
 
 export default App;
+
